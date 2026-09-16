7dd7345576bbfaf2968d3e94dd26da4ca73202c9
DjRober <smile.roberto3@gmail.com>
2026-09-16 15:14:38 -0600

    chore(app): eliminar ruta hello placeholder e import de Flask sin uso

--- run.py
+++ run.py
@@ -3,9 +3,5 @@ from app import create_app
 
 app = create_app()
 
-@app.route("/")
-def hello():
-        return "Hello, world! - secureaudit_mx"
-
 if __name__ == "__main__": #Evitamos conflictos al importar
     app.run(debug=True)
